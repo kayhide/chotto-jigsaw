d8d976b7e5d9f408783c597ff23ddedaf9f5a1cd
kayhide <kayhide@gmail.com>
2019-10-31 16:19:21 +0900

    Update merge command spec

--- spec/models/merge_command_spec.rb
+++ spec/models/merge_command_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe MergeCommand, type: :model do
-  it "works" do
-    subject.mergee_id = 3
-    expect(subject.mergee_id).to eq 3
+  describe "attributes" do
+    it "works" do
+      subject.mergee_id = 3
+      expect(subject.mergee_id).to eq 3
+    end
   end
 end
