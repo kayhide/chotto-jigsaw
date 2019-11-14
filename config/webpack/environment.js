bfd2b488f04466c20b24ceae05653d3af824721d
kayhide <kayhide@gmail.com>
2019-11-14 14:47:55 +0900

    Drop typescript

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,15 +1,4 @@
 const { environment } = require("@rails/webpacker");
 const webpack = require("webpack");
-const typescript = require("./loaders/typescript");
 
-environment.plugins.prepend(
-  "Provide",
-  new webpack.ProvidePlugin({
-    $: "jquery/src/jquery",
-    jQuery: "jquery/src/jquery",
-    _: "lodash"
-  })
-);
-
-environment.loaders.prepend("typescript", typescript);
 module.exports = environment;
