1b2f35ef219041fdd027b5e1b95a2185f890b5c0
kayhide <kayhide@gmail.com>
2019-11-15 00:33:42 +0900

    Rename app/javascript to app/frontend

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -1,6 +1,6 @@
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
+// a relevant structure within app/frontend and only use these pack files to reference
 // that code so it'll be compiled.
 
 // Uncomment to copy all static images under ../images to the output folder and reference
