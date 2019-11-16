6f8543db5597de6a4f3741f7a399b1ccaacc8963
kayhide <kayhide@gmail.com>
2019-11-16 14:42:57 +0900

    Update puzzles#new ui

--- config/application.rb
+++ config/application.rb
@@ -43,5 +43,8 @@ module ChottoJigsaw
     config.action_view.field_error_proc = Proc.new do |html_tag, instance|
       html_tag.html_safe
     end
+
+    config.i18n.available_locales = [:en, :ja]
+    config.i18n.default_locale = :ja
   end
 end
