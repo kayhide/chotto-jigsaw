4231c09f9e14fd7774e33057198fae2c7911d261
Hideaki Kawai <kayhide@gmail.com>
2019-11-26 16:24:06 +0900

    Livereload by webpack-dev-server on update of app

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,16 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
+const { resolve } = require('path');
+
+environment.config.merge({
+  devServer: {
+    contentBase: [
+      environment.config.devServer.contentBase,
+      resolve('./app')
+    ],
+    watchContentBase: true,
+  }
+});
 
 module.exports = environment.toWebpackConfig()
