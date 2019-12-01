e22a32d547f07ade5f81a4e26c2aae73a0e77d35
Hideaki Kawai <kayhide@gmail.com>
2019-12-01 12:59:49 +0900

    Load background picture smoothly

--- app/frontend/packs/background-picture.js
+++ app/frontend/packs/background-picture.js
@@ -1,13 +1,23 @@
-import $ from "jquery";
-
-
 export default class BackgroundPicture {
   static init() {
-    const $elm = $(".background-picture");
-    const url = $elm.data("picture");
-    console.log(url);
-    if (url) {
-      $elm.css("background-image", `url("${url}")`);
+    const elms = document.querySelectorAll(".background-picture .bg-image");
+    const next = (i) => {
+      const elm = elms[i];
+      if (elm) {
+        const url = elm.dataset.src;
+        if (url) {
+          const img = new Image();
+          img.onload = () => {
+            elm.classList.add("loaded");
+            elm.style.backgroundImage = `url(${url})`;
+            window.setTimeout(() => next(i + 1), 1000);
+          };
+          img.src = elm.dataset.src;
+        } else {
+          next(i + 1);
+        }
+      }
     }
+    next(0);
   }
 };
