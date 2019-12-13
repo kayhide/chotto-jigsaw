5e3744ad52f9faed5f5ad9d31f42d3f2a7a6b20a
Hideaki Kawai <kayhide@gmail.com>
2019-12-14 06:28:11 +0900

    Update FireRecord config

--- app/fire_record/fire_record/client.rb
+++ app/fire_record/fire_record/client.rb
@@ -9,6 +9,19 @@ module FireRecord
       @config = Rails.application.config_for(path).with_indifferent_access
       if @config.key?(:emulator_port)
         @config[:emulator_host] = [@config[:emulator_host], @config.delete(:emulator_port)].join(':')
+      elsif ENV.key?("GOOGLE_APPLICATION_CREDENTIALS")
+        creds = ENV["GOOGLE_APPLICATION_CREDENTIALS"]
+        if !File.exist?(creds)
+          begin
+            JSON.parse(creds)
+            path = File.expand_path("~/.google_application_credentials.json")
+            open(path, "w") do |io|
+              io << creds
+            end
+            ENV["GOOGLE_APPLICATION_CREDENTIALS"] = path
+          rescue
+          end
+        end
       end
       Google::Cloud::Firestore.new(**@config.symbolize_keys)
     end
