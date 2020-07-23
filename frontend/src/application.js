d980c3d47baeb286c45ec08744a1a2d875468289
Hideaki Kawai <kayhide@gmail.com>
2020-07-23 21:03:29 +0900

    Move styles and images

--- frontend/src/application.js
+++ frontend/src/application.js
@@ -1,7 +1,7 @@
 import "@fortawesome/fontawesome-free/css/fontawesome.css";
 import "@fortawesome/fontawesome-free/css/solid.css";
 import "@fortawesome/fontawesome-free/css/regular.css";
-import "../styles/application.css";
+import "./styles/application.css";
 
 import Main from "./Main.purs";
 Main.main();
