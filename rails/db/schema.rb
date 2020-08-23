d6677a027c885b10878ee6ba1394559b635d7f50
Hideaki Kawai <kayhide@gmail.com>
2020-08-23 23:10:46 +0900

    Add boundary to api responses of puzzles api

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_03_041905) do
+ActiveRecord::Schema.define(version: 2020_08_23_134641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,10 @@ ActiveRecord::Schema.define(version: 2020_05_03_041905) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "difficulty", null: false
     t.integer "pieces_count"
+    t.float "boundary_x"
+    t.float "boundary_y"
+    t.float "boundary_width"
+    t.float "boundary_height"
     t.index ["user_id"], name: "index_puzzles_on_user_id"
   end
 
