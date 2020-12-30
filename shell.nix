97ad54a2bc7a86a97954a3e43ae6eb70fd451c26
Hideaki Kawai <kayhide@gmail.com>
2020-12-30 10:20:54 +0900

    Upgrade ruby to 2.6.6

--- shell.nix
+++ shell.nix
@@ -1,19 +1,49 @@
-with import ./. {};
+{ overlays ? []
+}@args:
+
+let
+  inherit (nixpkgs) pkgs lib;
+
+  nodejs-overlay = self: super: {
+    my-nodejs = self.nodejs-14_x;
+    my-yarn = super.yarn.override {
+      nodejs = self.my-nodejs;
+    };
+  };
+
+  env-overlay = self: super: {
+    my-env = super.buildEnv {
+      name = "my-env";
+      paths = with self; [
+        docker-compose
+        gnumake
+
+        my-nodejs
+        my-yarn
+        purescript
+        spago
+
+        zlib
+        ruby.devEnv
+
+        postgresql_12
+      ];
+    };
+  };
+
+  nixpkgs = import <nixpkgs> (args // {
+    overlays = overlays ++ [
+      nodejs-overlay
+      env-overlay
+    ];
+  });
+
+in
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
-    docker-compose
-    gnumake
-
-    nodejs
-    yarn
-    purescript
-    spago
-
-    libiconv
+    my-env
     zlib
-    ruby_2_6
-
-    postgresql_12
+    libiconv
   ];
 }
