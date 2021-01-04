c85234f55ac2bcb01222212955b5b7d67568e02c
Hideaki Kawai <kayhide@gmail.com>
2021-01-04 16:52:58 +0900

    Mount app directory read-only

--- shell.nix
+++ shell.nix
@@ -2,6 +2,12 @@
 }@args:
 
 let
+  nixpkgs-src = builtins.fetchTarball {
+    # master of 2020-11-21.
+    url = "https://github.com/NixOS/nixpkgs/archive/2247d824fe07f16325596acc7faa286502faffd1.tar.gz";
+    sha256 = "sha256:09jzdnsq7f276cfkmynqiaqg145k8z908rlr0170ld1sms1a83rw";
+  };
+
   inherit (nixpkgs) pkgs lib;
 
   nodejs-overlay = self: super: {
@@ -22,6 +28,7 @@ let
         my-yarn
         purescript
         spago
+        python # node-grpc
 
         zlib
         ruby.devEnv
@@ -31,7 +38,7 @@ let
     };
   };
 
-  nixpkgs = import <nixpkgs> (args // {
+  nixpkgs = import nixpkgs-src (args // {
     overlays = overlays ++ [
       nodejs-overlay
       env-overlay
